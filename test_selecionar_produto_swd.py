1d65d525af094a35bbabe587b1b6ba4294576aae
juniormaza <junior_maza16@hotmail.com>
2024-04-12 17:21:56 -0300

    Este teste adiciona as seguintes funcionalidades no site Saucedemo: - Realização de login com sucesso. - Adição de um produto ao carrinho e conclusão da compra. - Limpeza do carrinho após a conclusão da compra. - Logout da conta do usuário.

--- test_selecionar_produto_swd.py
+++ test_selecionar_produto_swd.py
@@ -31,4 +31,24 @@ class Teste_produtos():
         assert self.driver.find_element(By.ID, "item_4_title_link").text == "Sauce Labs Backpack" 
          # confirma o preço da mochila
         assert self.driver.find_element(By.CSS_SELECTOR, ".inventory_item:nth-child(1) .inventory_item_price").text == "$29.99"
+        # clica no botão add to cart
+        self.driver.find_element(By.ID, "add-to-cart-sauce-labs-backpack").click()
+        # clica no carrinho de compras
+        self.driver.find_element(By.CSS_SELECTOR, "[data-test='shopping-cart-link']").click()
+        
+        # transição de página
 
+        # confima se está escrito Yoor Cart
+        assert self.driver.find_element(By.CSS_SELECTOR, "[data-test='title']").text == "Your Cart"
+        # valida quantidade de produtos no carrinho
+        assert self.driver.find_element(By.CSS_SELECTOR, "[data-test='shopping-cart-badge']").text == "1"
+        # confirma se é a mochila
+        assert self.driver.find_element(By.CSS_SELECTOR, "[data-test='inventory-item-name']").text == "Sauce Labs Backpack" 
+        # confirma o preço da mochila
+        assert self.driver.find_element(By.CSS_SELECTOR, "[data-test='inventory-item-price']").text == "$29.99"
+        # clica no botão remover item do carrinho
+        self.driver.find_element(By.CSS_SELECTOR, "[data-test='remove-sauce-labs-backpack']").click()
+        # clica o menu hamburguer 
+        self.driver.find_element(By.ID, "react-burger-menu-btn").click()
+        # clicar no logout/encerrar seção
+        self.driver.find_element(By.CSS_SELECTOR, "[data-test='logout-sidebar-link']").click()
